3dc5caa79ff63a673948ad003e013992cd57bd4c
martinasartori97 <martinasartori9@gmail.com>
2024-09-30 19:30:47 +0200

    edit js

--- index.js
+++ index.js
@@ -1,37 +1,31 @@
 // chiedere all'utente  il numero di chilometri che vuole percorrere
 let chilometri = prompt("quanti chilometri vuoi percorrere?");
-chilometri = Number (chilometri);
+chilometri = Number(chilometri);
+console.log(80);
 console.log(chilometri);
 
 
 // chiedere l'età del passeggero
 let eta = prompt("qual è la tua età?");
-eta = Number (eta);
+console.log(26);
+eta = Number(eta);
 console.log(eta);
 
 // prezzo per chilometro
 const prezzoalkm = 0.21;
 
 // calcolo del prezzo totale
-const prezzoalkm = 0.21;
-const distanza = Number
-const prezzototale = Number
-console.log(prezzototale);
+let prezzototale = chilometri * prezzoalkm
 
+// applicare uno sconto del 20% sui minorenni
+// applicare uno sconto del 20% sui minorenni e del 40% per gli over 65
+if eta(< 18:
+    sconto = 0.20   Sconto del 20 % per i minorenni)
+   eta(> 65:
+        sconto = 0.40   Sconto del 40 % per gli over 65)
 
-
-// applicare uno sconto del 20% sui minorenni 
-const prezzototale = 100;
-const percentualesconto = prezzototale * (percentualesconto / 100 );
-const prezzofinale = prezzototale - percentualesconto;
-
-if (eta > 18 && eta <65 || eta ==18){
-    prezzototale = chilometri * prezzoalkm
-}
-else if (eta > 18 && eta < 65){
-
-}
-
+else (sconto = 0.0   Nessuno sconto)
+// applicare uno sconto del 40% per gli over 65
 
 
 
